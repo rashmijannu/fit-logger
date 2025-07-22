7f09c13b1b5b394b511ef344ff6daefa0fdb888a
rashmi <rashmicjannu@gmail.com>
2025-07-22 22:50:09 +0530

    first commit

---
+++ src/main/java/com/fitness/fit_logger/FitLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.fit_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/fit_logger/FitLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.fit_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
